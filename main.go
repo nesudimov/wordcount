90954dd91d56886ff4729aaa0ed13a86ab524013
enesudimov <enesudimov@baltbet.ru>
2022-06-10 17:12:06 +0300

    Check return in if block

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ func main() {
 	str := os.Args[1]
 	if str == "" {
 		fmt.Printf("%d", 0)
-	} else {
-		words := strings.Split(str, " ")
-		fmt.Printf("%d", len(words))
+		return
 	}
+	words := strings.Split(str, " ")
+	fmt.Printf("%d", len(words))
 }
